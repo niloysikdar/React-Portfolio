101e6065667d0a066c7e72c93e306f47a3d641dc
niloysikdar <niloysikdar30@gmail.com>
2021-08-23 23:36:21 +0530

    Added HomePage

---
+++ src/pages/Home/LinksData.tsx
@@ -0,0 +1,37 @@
+import { ImGithub, ImLinkedin, ImAndroid } from "react-icons/im";
+import { FaMedium } from "react-icons/fa";
+import { SiInstagram } from "react-icons/si";
+
+interface IconsList {
+  title: string;
+  link: string;
+  icon: any;
+}
+
+export const LinksData: IconsList[] = [
+  {
+    title: "GitHub",
+    icon: <ImGithub color="#000000dc" />,
+    link: "https://github.com",
+  },
+  {
+    title: "LinkedIn",
+    icon: <ImLinkedin color="#000000dc" />,
+    link: "https://linkedin.com",
+  },
+  {
+    title: "Google Play",
+    icon: <ImAndroid color="#000000dc" />,
+    link: "https://play.google.com",
+  },
+  {
+    title: "Medium",
+    icon: <FaMedium color="#000000dc" />,
+    link: "https://medium.com",
+  },
+  {
+    title: "Instagram",
+    icon: <SiInstagram color="#000000dc" />,
+    link: "https://instagram.com",
+  },
+];
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,23 @@
 import styles from "./home.module.scss";
+// @ts-ignore
+import { LinksData } from "./LinksData.tsx";
+import profilePic from "../../assets/profile.jpg";
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <h2>Home</h2>
+      <img src={profilePic} alt="Profile" className={styles.image} />
+      <h2 className={styles.title}>Hey, Niloy here</h2>
+      <h3 className={styles.about}>
+        A Developer who loves to build cool things
+      </h3>
+      <div className={styles.links}>
+        {LinksData.map((item: { title: string; link: string; icon: any }) => (
+          <a href={item.link} target="_blank" rel="noreferrer" key={item.title}>
+            {item.icon}
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
