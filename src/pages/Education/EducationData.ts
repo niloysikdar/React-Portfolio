661f9b3995e09707144ee11e367b9899bbc89eb3
niloysikdar <niloysikdar30@gmail.com>
2021-08-24 16:35:25 +0530

    Added Education Section

---
+++ src/pages/Education/EducationData.ts
@@ -0,0 +1,22 @@
+export const EducationData = [
+  {
+    title: "B.Tech, CSE",
+    date: "2019-2023",
+    details: `Currently pursuing my B.Tech in CSE from Jalpaiguri Government Engineering College, 8.05 CGPA till now.
+      Highly active in the domains of Web and Mobile app development.`,
+  },
+
+  {
+    title: "12th Board, PCMB",
+    date: "2017-2019",
+    details:
+      "Finished 12th board exams from Islampur High School (H.S.) with 94% marks in PCMB.",
+  },
+
+  {
+    title: "10th Board",
+    date: "2017",
+    details:
+      "Finished 10th board exams from Islampur High School (H.S.) with 96% marks.",
+  },
+];
--- src/pages/Education/index.tsx
+++ src/pages/Education/index.tsx
@@ -1,7 +1,28 @@
+import { EducationCard } from "../../components/EducationCard/EducationCard";
+
+import { EducationData } from "./EducationData";
+import styles from "./education.module.scss";
+
+const eduOpen = "<education>";
+const eduClose = "<education/>";
+
 const Education = () => {
   return (
-    <div>
-      <h2>Education</h2>
+    <div className={styles.education}>
+      <div className={styles.wrapper}>
+        <h3 className={styles.eduOpen}>{eduOpen}</h3>
+        <div className={styles.center_line}></div>
+        {EducationData.map((item, index) => (
+          <EducationCard
+            key={item.date}
+            title={item.title}
+            date={item.date}
+            details={item.details}
+            isLeft={index % 2 === 0 ? true : false}
+          />
+        ))}
+        <h3 className={styles.eduClose}>{eduClose}</h3>
+      </div>
     </div>
   );
 };
