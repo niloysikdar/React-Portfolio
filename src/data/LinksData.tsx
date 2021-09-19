6a15edcd44debede2769930fd3745ce19f8d4497
niloysikdar <niloysikdar30@gmail.com>
2021-09-20 01:40:09 +0530

    Updated HomePage Links

--- src/data/LinksData.tsx
+++ src/data/LinksData.tsx
@@ -6,31 +6,31 @@ export const LinksData = [
   {
     title: "GitHub",
     icon: <ImGithub color="#000000dc" />,
-    link: "https://github.com",
+    link: "https://github.com/niloysikdar",
   },
   {
     title: "LinkedIn",
     icon: <ImLinkedin color="#000000dc" />,
-    link: "https://linkedin.com",
+    link: "https://www.linkedin.com/in/niloysikdar",
   },
   {
     title: "Google Play",
     icon: <ImAndroid color="#000000dc" />,
-    link: "https://play.google.com",
+    link: "https://play.google.com/store/apps/dev?id=6194928995061750437",
   },
   {
     title: "Medium",
     icon: <FaMedium color="#000000dc" />,
-    link: "https://medium.com",
-  },
-  {
-    title: "Instagram",
-    icon: <SiInstagram color="#000000dc" />,
-    link: "https://instagram.com",
+    link: "https://niloysikdar.medium.com",
   },
   {
     title: "Twitter",
     icon: <FaTwitter color="#000000dc" />,
-    link: "https://twitter.com",
+    link: "https://twitter.com/niloysikdar_",
+  },
+  {
+    title: "Instagram",
+    icon: <SiInstagram color="#000000dc" />,
+    link: "https://www.instagram.com/niloy_sikdar/",
   },
 ];
