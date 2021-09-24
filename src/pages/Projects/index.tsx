12b0d20fc168730b73e829345ae5e3d8d6a79379
imniladri <jnvniladri101@gmail.com>
2021-09-24 20:13:53 +0530

    Removed framer from projects

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,12 +1,6 @@
-import { motion } from 'framer-motion';
 import { ProjectCard } from '../../components/ProjectCard/ProjectCard';
 import { ProjectsData } from '../../data/ProjectsData';
 import styles from './projects.module.scss';
-
-import {
-    pageVariants,
-    pageTransition,
-} from '../../components/Framer/Animation';
 
 // @ts-ignore
 import Flip from 'react-reveal/Flip';
@@ -17,47 +11,39 @@ const projectsClose = '</projects>';
 const Projects = () => {
   return (
     <div className={styles.projects}>
-      <motion.div 
-        initial='init'
-        animate='anim'
-        exit='last'
-        variants={pageVariants}
-        transition={pageTransition}
-      >
-        <div className={styles.wrapper}>
-          <h3 className={styles.projectsOpen}>{projectsOpen}</h3>
+      <div className={styles.wrapper}>
+        <h3 className={styles.projectsOpen}>{projectsOpen}</h3>
 
-          <div className={styles.projects_content}>
-            {ProjectsData.map((item, index) =>
-              index % 2 === 0 ? (
-                <Flip top key={item.title}>
-                  <ProjectCard
-                    key={item.title}
-                    title={item.title}
-                    description={item.description}
-                    image={item.image}
-                    GitHub={item.GitHub}
-                    hosted={item.hosted}
-                  />
-                </Flip>
-              ) : (
-                <Flip bottom key={item.title}>
-                  <ProjectCard
-                    key={item.title}
-                    title={item.title}
-                    description={item.description}
-                    image={item.image}
-                    GitHub={item.GitHub}
-                    hosted={item.hosted}
-                  />
-                </Flip>
-              )
-            )}
-          </div>
-
-          <h3 className={styles.projectsClose}>{projectsClose}</h3>
+        <div className={styles.projects_content}>
+          {ProjectsData.map((item, index) =>
+            index % 2 === 0 ? (
+              <Flip top key={item.title}>
+                <ProjectCard
+                  key={item.title}
+                  title={item.title}
+                  description={item.description}
+                  image={item.image}
+                  GitHub={item.GitHub}
+                  hosted={item.hosted}
+                />
+              </Flip>
+            ) : (
+              <Flip bottom key={item.title}>
+                <ProjectCard
+                  key={item.title}
+                  title={item.title}
+                  description={item.description}
+                  image={item.image}
+                  GitHub={item.GitHub}
+                  hosted={item.hosted}
+                />
+              </Flip>
+            )
+          )}
         </div>
-      </motion.div>
+
+        <h3 className={styles.projectsClose}>{projectsClose}</h3>
+      </div>
     </div>
   );
 };
