79b784275c5683f0b63d77f5643052c60e8f537a
niloysikdar <niloysikdar30@gmail.com>
2021-09-24 12:41:40 +0530

    Modified Project Data

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -1,52 +1,42 @@
 export const ProjectsData = [
   {
-    title: "Project1",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          iure officia cum soluta beatae fugiat? Eum qui ipsum harum vero illum
-          autem repellat magni consectetur.Cumque, ex aut! Delectus, veniam.`,
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
-    GitHub: "https://www.github.com/",
-    hosted: "https://www.hosted.com/",
+    title: "RememBirth",
+    description: `Do you always forget everyone's Birthday and miss to wish them?
+    Don't worry, the solution is here.`,
+    image: "https://i.imgur.com/wpznb1x.png",
+    GitHub: "https://github.com/niloysikdar/RememBirth",
+    hosted: "https://remembirth.vercel.app/",
   },
   {
-    title: "Project2",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          iure officia cum soluta beatae fugiat? Eum qui ipsum harum vero illum
-          autem repellat magni consectetur.Cumque, ex aut! Delectus, veniam.`,
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
-    GitHub: "https://www.github.com/",
-    hosted: "https://www.hosted.com/",
+    title: "Plaso Connect",
+    description: `A mobile application, where people in need of blood plasma/oxygen can directly find and contact the donors and healthcare units.`,
+    image: "https://i.imgur.com/n6nVFnT.png",
+    GitHub: "https://github.com/niloysikdar/Plaso-Connect",
+    hosted:
+      "https://devlibrary.withgoogle.com/products/flutter/repos/niloysikdar-Plaso-Connect",
   },
   {
-    title: "Project3",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          iure officia cum soluta beatae fugiat? Eum qui ipsum harum vero illum
-          autem repellat magni consectetur.Cumque, ex aut! Delectus, veniam.`,
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
-    GitHub: "https://www.github.com/",
-    hosted: "https://www.hosted.com/",
+    title: "Smile-E",
+    description: `A one-stop solution for an easy, digital, and trusted agreement for NGOs using eSignature.
+    Added a dedicated Dashboard section to showcase the NGOs’ past works for easy fundraising.`,
+    image: "https://i.imgur.com/EqpR2Ri.png",
+    GitHub: "https://github.com/niloysikdar/Smile-E",
+    hosted: "https://devpost.com/software/smile-e",
   },
   {
-    title: "Project4",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          iure officia cum soluta beatae fugiat? Eum qui ipsum harum vero illum
-          autem repellat magni consectetur.Cumque, ex aut! Delectus, veniam.`,
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
-    GitHub: "https://www.github.com/",
-    hosted: "https://www.hosted.com/",
+    title: "CodeChef Peek",
+    description: `Mobile application to view the statistics of CodeChef Users with just one click.
+    Downloaded by 200+ users.`,
+    image: "https://i.imgur.com/IkaBaNN.png",
+    GitHub: "https://github.com/niloysikdar/CodeChef-Peek",
+    hosted:
+      "https://play.google.com/store/apps/details?id=com.codechef.peek.android",
   },
   {
-    title: "Project5",
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-          iure officia cum soluta beatae fugiat? Eum qui ipsum harum vero illum
-          autem repellat magni consectetur.Cumque, ex aut! Delectus, veniam.`,
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png",
-    GitHub: "https://www.github.com/",
-    hosted: "https://www.hosted.com/",
+    title: "Sell It",
+    description: `Platform for selling and buying new and old/used products, used Golang, Gorilla Mux, SQLite and Vue.js`,
+    image: "https://i.imgur.com/6YH6XOC.png",
+    GitHub: "https://github.com/niloysikdar/Sell-It",
+    hosted: "https://github.com/niloysikdar/Sell-It#screenshots-",
   },
 ];
