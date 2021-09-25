432023f7d31646de91b0f5064600e78b62f1a07b
niloysikdar <niloysikdar30@gmail.com>
2021-09-26 00:24:09 +0530

    Updated ProjectsData

--- src/data/ProjectsData.ts
+++ src/data/ProjectsData.ts
@@ -12,8 +12,7 @@ export const ProjectsData = [
     description: `A mobile application, where people in need of blood plasma/oxygen can directly find and contact the donors and healthcare units.`,
     image: 'https://i.imgur.com/n6nVFnT.png',
     GitHub: 'https://github.com/niloysikdar/Plaso-Connect',
-    hosted:
-      'https://devlibrary.withgoogle.com/products/flutter/repos/niloysikdar-Plaso-Connect',
+    hosted: `https://devlibrary.withgoogle.com/products/flutter/repos/niloysikdar-Plaso-Connect`,
   },
   {
     title: 'Smile-E',
@@ -29,8 +28,21 @@ export const ProjectsData = [
     Downloaded by 200+ users.`,
     image: 'https://i.imgur.com/IkaBaNN.png',
     GitHub: 'https://github.com/niloysikdar/CodeChef-Peek',
-    hosted:
-      'https://play.google.com/store/apps/details?id=com.codechef.peek.android',
+    hosted: `https://play.google.com/store/apps/details?id=com.codechef.peek.android`,
+  },
+  {
+    title: 'Pdf Chest',
+    description: `An application to search and get the list of available pdfs related to a topic. Download them for free.`,
+    image: 'https://i.imgur.com/gNfLR0a.png',
+    GitHub: 'https://github.com/niloysikdar/PdfChest',
+    hosted: `https://play.google.com/store/apps/details?id=com.pdfchest.android`,
+  },
+  {
+    title: 'WeShare',
+    description: `A Social Media Application using MERN stack with Google OAuth and JWT for login`,
+    image: 'https://imgur.com/d6dVSTx.png',
+    GitHub: 'https://github.com/niloysikdar/WeShare',
+    hosted: 'https://github.com/niloysikdar/Grow-Green#screenshots-',
   },
   {
     title: 'Sell It',
@@ -38,5 +50,19 @@ export const ProjectsData = [
     image: 'https://i.imgur.com/6YH6XOC.png',
     GitHub: 'https://github.com/niloysikdar/Sell-It',
     hosted: 'https://github.com/niloysikdar/Sell-It#screenshots-',
+  },
+  {
+    title: 'Sukoon',
+    description: `A platform to help people suffering from mental health issues and make them feel better through customized activities suggested according to their needs and also make them aware of their mental health condition.`,
+    image: 'https://i.imgur.com/1OYZOT7.png',
+    GitHub: 'https://github.com/niloysikdar/Sukoon',
+    hosted: 'https://sukoon.vercel.app/',
+  },
+  {
+    title: 'Grow Green',
+    description: `A mobile app to bring more customers to the small businesses that have negatively been impacted by COVID-19`,
+    image: 'https://i.imgur.com/S52NuUH.png',
+    GitHub: 'https://github.com/niloysikdar/Grow-Green',
+    hosted: 'https://github.com/niloysikdar/Grow-Green#screenshots-',
   },
 ];
